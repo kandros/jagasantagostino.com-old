9506472053cd6af4938cad8acf50ce33c03c9503
Jaga Santagostino <jagasantagostino@gmail.com>
2017-04-09 15:08:45 +0200

    add email in social

--- components/socials.js
+++ components/socials.js
@@ -7,6 +7,7 @@ const Socials = () => (
         <SocialIcon style={{marginLeft: 16}} color="black" url="https://github.com/kandros"/>
         <SocialIcon style={{marginLeft: 16}} color="black" url="https://www.linkedin.com/in/jaga-santagostino-500676b0/"/>
         <SocialIcon style={{marginLeft: 16}} color="black" network="medium" url="http://blog.jagasantagostino.com/"/>
+        <SocialIcon style={{marginLeft: 16}} color="black" network="email" url="mailto:info@jagasantagostino.com"/>
     </div>
 )
 
