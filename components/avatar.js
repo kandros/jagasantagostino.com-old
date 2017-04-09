d6ee750d18e9b34f945ec5cb90ecdc42cb3687b3
Jaga Santagostino <jagasantagostino@gmail.com>
2017-04-09 17:50:37 +0200

    image loading placeholder and fixed width

--- components/avatar.js
+++ components/avatar.js
@@ -4,9 +4,11 @@ const Avatar = ({src}) => (
     <div style={{zIndex: 10}}>
         <style jsx>{`
         img {
+            background: #f7f7f7;
             max-width: 100%;
             display: block;
             border-radius: 2px;
+            width: 230px;
             height: 230px;
             box-shadow: 13px 14px 20px 0px rgba(0, 0, 0, 0.22)
         }
